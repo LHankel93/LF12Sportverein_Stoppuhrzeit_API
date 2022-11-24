74c35169c417404bfed1cc82cea7eaa3499e2fbb
Lorenz Hankel <lorenz.hankel@yahoo.de>
2022-11-24 22:05:13 +0100

    Update test_post.php
    - test_post.php angepasst um sowohl mit json objekt, als auch mit urlencoded umgehen zu können.

--- view/test_post.php
+++ view/test_post.php
@@ -1,29 +1,29 @@
 <?php
 
 // Takes raw data from the request
+
 $json = file_get_contents('php://input');
 
 // Converts it into a PHP object
 $data = json_decode($json);
-
-foreach($data as $jsondata){
-    echo $jsondata;
-}
-
-/*
-// FOLGENDES FUNKTIONIERT NUR MIT URLENCODED RAW, NICHT MIT JSON OBJEKTEN!
-// Prüfen ob POST gesetzt und gefüllt ist.
-if (isset($_POST)){
-    // Durch alle W erte im POST iterieren und ausgeben auf Seite zum Testen.
-    foreach($_POST as $value){
-        if (is_array($value)){
-            foreach($value as $level1){
-                echo "$level1";
-            }
-        }
-        echo "$value";
+if (isset($data)) {
+    foreach ($data as $jsondata) {
+        echo $jsondata;
     }
 } else {
-    echo ("POST ist leer.");
+    // FOLGENDES FUNKTIONIERT NUR MIT URLENCODED RAW, NICHT MIT JSON OBJEKTEN!
+    // Prüfen ob POST gesetzt und gefüllt ist.
+    if (isset($_POST)) {
+        // Durch alle W erte im POST iterieren und ausgeben auf Seite zum Testen.
+        foreach ($_POST as $value) {
+            if (is_array($value)) {
+                foreach ($value as $level1) {
+                    echo "$level1";
+                }
+            }
+            echo "$value";
+        }
+    } else {
+        echo ("POST ist leer.");
+    }
 }
-*/
